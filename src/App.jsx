c290fc995b9995a30389bc5bac146f16ad6236b9
Nabi-Ameen <nabiameen35126@gmail.com>
2026-04-06 17:04:30 +0500

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Card from "./components/card";
+import { useState } from "react";
 import microImage from "./assets/microsoft.jpg"
+import { RxCross2 } from "react-icons/rx";
 function App() {
 
   const items = [
@@ -29,15 +31,33 @@ function App() {
     }
   ]
 
+  const [isOpen, SetIsOpen] = useState(false);
+
   return (
-    <div className="flex gap-8">
-      {items.map((item, index) => {
-        return (
-          <div key={index}>
-            <Card item={item} />
-          </div>
-        )
-      })}
+    <div>
+
+      {isOpen && 
+        <div className="bg-black/55 h-screen w-100 fixed inset-0 flex items-center justify-center">
+        <div className="w-1/2 h-[600px] bg-white rounded-lg ">
+          <RxCross2 size={34} className="float-right m-4" onClick={()=> SetIsOpen(false)} />
+        </div>
+      </div>
+      }
+
+      <button onClick={() => SetIsOpen(true)} className="bg-blue-700 text-white rounded-md p-4 float-right">Login In</button>
+      <div className="flex gap-8 ml-60 mt-10">
+
+
+        {items.map((item, index) => {
+          return (
+            <div key={index}>
+
+
+              <Card item={item} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Button from './Button'
 
 const Card = ({ item }) => {
+  
+  
 
   return (
     <div className='p-4 w-60 bg-gray-300'>
+
+      
       <div><img src={item.image} alt={item.title} /></div>
       <h5 className='font-bold text-lg py-4'>{item.title}</h5>
       <p className='font-normal text-lg py-4'>{item.desc}</p>

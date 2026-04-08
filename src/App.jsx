e5fbf497f637777c5e266be3345f14168f107ae1
Nabi-Ameen <nabiameen35126@gmail.com>
2026-04-08 16:15:08 +0500

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,60 +4,103 @@ import microImage from "./assets/microsoft.jpg"
 import { RxCross2 } from "react-icons/rx";
 function App() {
 
-  const items = [
-    {
-      image: microImage,
-      title: "Designed for life today – and tomorrow",
-      desc: "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love.",
-      btnText: "See if your PC is ready",
-    },
-    {
-      image: microImage,
-      title: "Bring the world closer with Bing Wallpaper",
-      desc: "Download the free app and enjoy breathtaking views with a new background each day.",
-      btnText: "Get wallpaper",
-    },
-    {
-      image: microImage,
-      title: "Microsoft Edge",
-      desc: "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love.",
-      btnText: "See if your PC is ready",
-    },
-    {
-      image: microImage,
-      title: "Microsoft OneDrive",
-      desc: "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love.",
-      btnText: "See if your PC is ready",
-    }
-  ]
 
-  const [isOpen, SetIsOpen] = useState(false);
+
+  // firstName,
+  // lastName,
+  // email,
+  // rollNo,
+  //age,
+  //address,
+  //gender,
+  //subjects: 10 subject names select field
+
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState("");
+  const [language, setLanguage] = useState("");
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, age, gender, language)
+  }
+
 
   return (
-    <div>
-
-      {isOpen && 
-        <div className="bg-black/55 h-screen w-100 fixed inset-0 flex items-center justify-center">
-        <div className="w-1/2 h-[600px] bg-white rounded-lg ">
-          <RxCross2 size={34} className="float-right m-4" onClick={()=> SetIsOpen(false)} />
+    <div className="flex items-center justify-center mt-10">
+      <form className="w-1/2 bg-slate-300 rounded-md p-10 space-y-6">
+        <div className="flex flex-col">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            className="w-[400px] border rounded-lg p-2"
+          />
         </div>
-      </div>
-      }
+        <div className="flex flex-col">
+          <label htmlFor="name">Age</label>
+          <input
+            type="number"
+            name="age"
+            id="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="AGE"
+            className="w-[400px] border rounded-lg p-2" />
+        </div>
 
-      <button onClick={() => SetIsOpen(true)} className="bg-blue-700 text-white rounded-md p-4 float-right">Login In</button>
-      <div className="flex gap-8 ml-60 mt-10">
-
-
-        {items.map((item, index) => {
-          return (
-            <div key={index}>
-
-
-              <Card item={item} />
+        <div className="flex flex-col">
+          <p>Gender</p>
+          <div className="flex gap-9">
+            <div>
+              <input
+                type="radio"
+                name="gender"
+                id="male"
+                checked={gender === "male"}
+                onClick={(e) => setGender("male")} />
+              <label htmlFor="name">male</label>
             </div>
-          )
-        })}
-      </div>
+
+            <div>
+              <input
+                type="radio"
+                name="gender"
+                id="female"
+                checked={gender === "female"}
+                onClick={(e) => setGender("female")} />
+              <label htmlFor="female">female</label>
+            </div>
+
+
+          </div>
+          <div className="mt-10">
+
+            <select
+              name="language"
+              id="language"
+              className="w-[300px]"
+              onChange={(e)=> setLanguage(e.target.value)}
+            >
+              <option value="html">html</option>
+              <option value="css">css</option>
+              <option value="js">js</option>
+              <option value="reactjs">reactjs</option>
+            </select>
+          </div>
+
+
+        </div>
+
+        <button type="submit" onClick={handleSubmit} className="bg-blue-600 text-white px-12 py-4 rounded-lg">Save</button>
+      </form>
     </div>
   )
 }

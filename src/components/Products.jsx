5cf8946dcd166abbf14be5a0580f3b5aeeb35def
Nabi-Ameen <nabiameen35126@gmail.com>
2026-04-15 16:07:01 +0500

    show loader

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,30 +2,37 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 
 const Products = () => {
-    const [product, setProduct] = useState()
+    const [product, setProduct] = useState();
+    const [loading, setLoading] = useState(false)
 
 
-
-    console.log("product", product)
-
-    const handleApi = () => {
-        let response = axios.get("https://fakestoreapi.com/products")
-        response.then((item) => setProduct(item.data))
+    const getProducts = async () => {
+        setLoading(true)
+        let response = await axios.get("https://fakestoreapi.com/products")
+        setProduct(response.data)
+        setLoading(false)
 
         // console.log("res", res)
         // fetch("https://fakestoreapi.com/products").then((res) => res.json()).then((item) => setProduct(item))
+
     }
 
     useEffect(() => {
-        handleApi()
+        getProducts()
     }, [])
+
+    if (loading) {
+        return (
+            <div className='h-screen w-full flex items-center justify-center text-4xl font-bold'>
+                loading...
+            </div>
+        )
+    }
 
 
     return (
         <div className='h-screen'>
             {/* <button className='bg-green-900 px-8 py-3 rounded-lg text-white' onClick={handleApi}>Retrieve Data</button> */}
-
-
             <div className='grid grid-cols-4 gap-4'>
                 {
                     product?.map((item) => {
